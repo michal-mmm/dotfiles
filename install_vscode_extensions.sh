36e8f4debdb80fbd5549722b178ba8bdff85dd3d
michal-mmm <84588078+michal-mmm@users.noreply.github.com>
2024-06-04 08:49:59 +0200

    Create install_vscode_extensions.sh

---
+++ install_vscode_extensions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+code --install-extension aaron-bond.better-comments
+code --install-extension aykutsarac.jsoncrack-vscode
+code --install-extension eamodio.gitlens
+code --install-extension mechatroner.rainbow-csv
+code --install-extension ms-python.debugpy
+code --install-extension ms-python.python
+code --install-extension ms-python.vscode-pylance
+code --install-extension ms-toolsai.jupyter
+code --install-extension ms-toolsai.jupyter-keymap
+code --install-extension ms-toolsai.jupyter-renderers
+code --install-extension ms-toolsai.vscode-jupyter-cell-tags
+code --install-extension ms-toolsai.vscode-jupyter-slideshow
+code --install-extension njpwerner.autodocstring
+code --install-extension redhat.vscode-yaml
+code --install-extension shd101wyy.markdown-preview-enhanced
+code --install-extension tamasfe.even-better-toml
